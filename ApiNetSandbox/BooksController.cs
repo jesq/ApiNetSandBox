f48f0a02d3137e0ec1105e60a6d5eb19033f2513
Cosmin Jescu <57216949+jesq@users.noreply.github.com>
2021-09-03 10:36:57 +0300

    Added Books model & controller

---
+++ ApiNetSandbox/BooksController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiNetSandBox
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private Book[] books;
+
+        public BooksController()
+        {
+            books = new Book[2];
+            books[0] = new Book();
+            books[0].Id = 0;
+            books[0].Title = "Az isteni formula";
+            books[0].Language = "Hungarian";
+            books[0].Author = "Jose Rodrigues dos Santos";
+        }
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return books;
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
