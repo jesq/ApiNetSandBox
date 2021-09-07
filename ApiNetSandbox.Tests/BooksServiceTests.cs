7cecc8f0802d05a102178708e9282f076a4ebf73
Cosmin Jescu <57216949+jesq@users.noreply.github.com>
2021-09-07 11:59:03 +0300

    Added BooksService Tests

---
+++ ApiNetSandbox.Tests/BooksServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using ApiNetSandBox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ApiNetSandbox.Tests
+{
+    public class BooksServiceTests
+    {
+        [Fact]
+        public void ShouldHaveLondonCoordinates()
+        {
+
+            // Assume
+            var booksService = new BooksService();
+
+
+
+            // Act
+            booksService.Post(new Book
+            {
+                Title = "Test Book Nr1",
+                Language = "English",
+                Author = "Some Author"
+            });
+            booksService.Delete(2);
+            booksService.Post(new Book
+            {
+                Title = "Test Book Nr2",
+                Language = "English",
+                Author = "Some Author2"
+            });
+
+            // Assert
+            Assert.Equal("Test Book Nr1", booksService.Get(3).Title);
+        }
+    }
+}
