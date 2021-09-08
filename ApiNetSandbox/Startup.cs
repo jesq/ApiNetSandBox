3e8046f04eb57fc8af853af189eae865505a9477
Cosmin Jescu <57216949+jesq@users.noreply.github.com>
2021-09-08 12:45:55 +0300

    Added documentation to swagger UI

--- ApiNetSandbox/Startup.cs
+++ ApiNetSandbox/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -31,6 +33,10 @@ namespace ApiNetSandBox
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiSandbox", Version = "v1" });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
             });
             services.AddSingleton<IBooksService, BooksService>();
         }
